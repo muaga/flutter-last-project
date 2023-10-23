b5bb2aa4e784f42540a64315eff2b7135e33c974
muaga <more9494@naver.com>
2023-10-23 18:44:50 +0900

    icon 수정

--- lib/_core/constants/icon.dart
+++ lib/_core/constants/icon.dart
@@ -2,87 +2,87 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blog/_core/constants/color.dart';
 
-Icon iconArrowBack({Color mColor = kFontBlack}) {
-  return Icon(Icons.arrow_back_ios, color: mColor, size: 25);
+Icon iconArrowBack({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(Icons.arrow_back_ios, color: mColor, size: mSize);
 }
 
-Icon iconArrowForward({Color mColor = kFontBlack}) {
-  return Icon(Icons.arrow_forward_ios, color: mColor, size: 25);
+Icon iconArrowForward({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(Icons.arrow_forward_ios, color: mColor, size: mSize);
 }
 
-Icon iconWriteMenu({Color mColor = kFontBlack}) {
-  return Icon(Icons.edit_note, color: mColor, size: 25);
+Icon iconWriteMenu({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(Icons.edit_note, color: mColor, size: mSize);
 }
 
-Icon iconEmptyStar({Color mColor = kFontBlack}) {
-  return Icon(Icons.star_border_rounded, color: mColor, size: 25);
+Icon iconEmptyStar({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(Icons.star_border_rounded, color: mColor, size: mSize);
 }
 
-Icon iconFullStar({Color mColor = kFontBlack}) {
-  return Icon(Icons.star_rounded, color: mColor, size: 25);
+Icon iconFullStar({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(Icons.star_rounded, color: mColor, size: mSize);
 }
 
-Icon iconEmptyHeart({Color mColor = kFontBlack}) {
-  return Icon(Icons.favorite_border_rounded, color: mColor, size: 25);
+Icon iconEmptyHeart({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(Icons.favorite_border_rounded, color: mColor, size: mSize);
 }
 
-Icon iconFullHeart({Color mColor = kFontBlack}) {
-  return Icon(Icons.favorite_rounded, color: mColor, size: 25);
+Icon iconFullHeart({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(Icons.favorite_rounded, color: mColor, size: mSize);
 }
 
-Icon iconPost({Color mColor = kFontBlack}) {
-  return Icon(Icons.edit_square, color: mColor, size: 25);
+Icon iconPost({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(Icons.edit_outlined, color: mColor, size: mSize);
 }
 
-Icon iconReply({Color mColor = kFontBlack}) {
-  return Icon(CupertinoIcons.ellipses_bubble, color: mColor, size: 25);
+Icon iconReply({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(Icons.comment_outlined, color: mColor, size: mSize);
 }
 
-Icon iconDotMenu({Color mColor = kFontBlack}) {
-  return Icon(Icons.more_vert_sharp, color: mColor, size: 25);
+Icon iconDotMenu({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(Icons.more_vert_sharp, color: mColor, size: mSize);
 }
 
-Icon iconHamburgerMenu({Color mColor = kFontBlack}) {
-  return Icon(Icons.density_medium_rounded, color: mColor, size: 25);
+Icon iconHamburgerMenu({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(Icons.density_medium_rounded, color: mColor, size: mSize);
 }
 
-Icon iconSearch({Color mColor = kFontBlack}) {
-  return Icon(CupertinoIcons.search, color: mColor, size: 25);
+Icon iconSearch({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(CupertinoIcons.search, color: mColor, size: mSize);
 }
 
-Icon iconPhoto({Color mColor = kFontBlack}) {
-  return Icon(Icons.photo_camera, color: mColor, size: 25);
+Icon iconPhoto({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(Icons.photo_camera, color: mColor, size: mSize);
 }
 
-Icon iconClose({Color mColor = kFontBlack}) {
-  return Icon(Icons.close, color: mColor, size: 25);
+Icon iconClose({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(Icons.close, color: mColor, size: mSize);
 }
 
-Icon iconPause({Color mColor = kFontBlack}) {
-  return Icon(Icons.pause, color: mColor, size: 25);
+Icon iconPause({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(Icons.pause, color: mColor, size: mSize);
 }
 
-Icon iconBookMark({Color mColor = kFontBlack}) {
-  return Icon(Icons.bookmark, color: mColor, size: 25);
+Icon iconBookMark({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(Icons.bookmark, color: mColor, size: mSize);
 }
 
-Icon iconBottomSearch({Color mColor = kFontBlack}) {
-  return Icon(Icons.manage_search, color: mColor, size: 25);
+Icon iconBottomSearch({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(Icons.manage_search, color: mColor, size: mSize);
 }
 
-Icon iconBottomSetting({Color mColor = kFontBlack}) {
-  return Icon(CupertinoIcons.profile_circled, color: mColor, size: 25);
+Icon iconBottomSetting({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(CupertinoIcons.profile_circled, color: mColor, size: mSize);
 }
 
-Icon iconBottomlibrary({Color mColor = kFontBlack}) {
-  return Icon(Icons.shelves, color: mColor, size: 25);
+Icon iconBottomlibrary({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(Icons.shelves, color: mColor, size: mSize);
 }
 
-Icon iconBottomFeed({Color mColor = kFontBlack}) {
-  return Icon(CupertinoIcons.today, color: mColor, size: 25);
+Icon iconBottomFeed({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(CupertinoIcons.today, color: mColor, size: mSize);
 }
 
 // TODO : 바텀네비게이션 - 투데이 아이콘 결정하기
-Icon iconBottomToday({Color mColor = kFontBlack}) {
-  return Icon(Icons.accessibility, color: mColor, size: 25);
+Icon iconBottomToday({Color mColor = kFontBlack, double mSize = 25}) {
+  return Icon(Icons.accessibility, color: mColor, size: mSize);
 }
