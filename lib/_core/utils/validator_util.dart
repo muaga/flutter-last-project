f5e27ab7540ac29d698e48c0a06114575fd52d5c
muaga <more9494@naver.com>
2023-10-23 16:55:01 +0900

    ㅍ미validate 추가

--- lib/_core/utils/validator_util.dart
+++ lib/_core/utils/validator_util.dart
@@ -54,3 +54,15 @@ Function validateTitle() {
     }
   };
 }
+
+Function validateContent() {
+  return (String? value) {
+    if (value!.isEmpty) {
+      return "내용은 공백이 들어갈 수 없습니다.";
+    } else if (value.length > 500) {
+      return "내용의 길이를 초과하였습니다.";
+    } else {
+      return null;
+    }
+  };
+}
