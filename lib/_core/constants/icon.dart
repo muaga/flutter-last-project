bcb644c97169ae4b75606c93179909d11e446d28
muaga <more9494@naver.com>
2023-10-23 14:21:19 +0900

    최초세팅

---
+++ lib/_core/constants/icon.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_blog/_core/constants/color.dart';
+
+Icon iconArrowBack({Color mColor = kFontBlack}) {
+  return Icon(Icons.arrow_back_ios, color: mColor, size: 25);
+}
+
+Icon iconArrowForward({Color mColor = kFontBlack}) {
+  return Icon(Icons.arrow_forward_ios, color: mColor, size: 25);
+}
+
+Icon iconWriteMenu({Color mColor = kFontBlack}) {
+  return Icon(Icons.edit_note, color: mColor, size: 25);
+}
+
+Icon iconEmptyStar({Color mColor = kFontBlack}) {
+  return Icon(Icons.star_border_rounded, color: mColor, size: 25);
+}
+
+Icon iconFullStar({Color mColor = kFontBlack}) {
+  return Icon(Icons.star_rounded, color: mColor, size: 25);
+}
+
+Icon iconEmptyHeart({Color mColor = kFontBlack}) {
+  return Icon(Icons.favorite_border_rounded, color: mColor, size: 25);
+}
+
+Icon iconFullHeart({Color mColor = kFontBlack}) {
+  return Icon(Icons.favorite_rounded, color: mColor, size: 25);
+}
+
+Icon iconPost({Color mColor = kFontBlack}) {
+  return Icon(Icons.edit_square, color: mColor, size: 25);
+}
+
+Icon iconReply({Color mColor = kFontBlack}) {
+  return Icon(CupertinoIcons.ellipses_bubble, color: mColor, size: 25);
+}
+
+Icon iconDotMenu({Color mColor = kFontBlack}) {
+  return Icon(Icons.more_vert_sharp, color: mColor, size: 25);
+}
+
+Icon iconHamburgerMenu({Color mColor = kFontBlack}) {
+  return Icon(Icons.density_medium_rounded, color: mColor, size: 25);
+}
+
+Icon iconSearch({Color mColor = kFontBlack}) {
+  return Icon(CupertinoIcons.search, color: mColor, size: 25);
+}
+
+Icon iconPhoto({Color mColor = kFontBlack}) {
+  return Icon(Icons.photo_camera, color: mColor, size: 25);
+}
+
+Icon iconClose({Color mColor = kFontBlack}) {
+  return Icon(Icons.close, color: mColor, size: 25);
+}
+
+Icon iconPause({Color mColor = kFontBlack}) {
+  return Icon(Icons.pause, color: mColor, size: 25);
+}
+
+Icon iconBookMark({Color mColor = kFontBlack}) {
+  return Icon(Icons.bookmark, color: mColor, size: 25);
+}
+
+Icon iconBottomSearch({Color mColor = kFontBlack}) {
+  return Icon(Icons.manage_search, color: mColor, size: 25);
+}
+
+Icon iconBottomSetting({Color mColor = kFontBlack}) {
+  return Icon(CupertinoIcons.profile_circled, color: mColor, size: 25);
+}
+
+Icon iconBottomlibrary({Color mColor = kFontBlack}) {
+  return Icon(Icons.shelves, color: mColor, size: 25);
+}
+
+Icon iconBottomFeed({Color mColor = kFontBlack}) {
+  return Icon(CupertinoIcons.today, color: mColor, size: 25);
+}
+
+// TODO : 바텀네비게이션 - 투데이 아이콘 결정하기
+Icon iconBottomToday({Color mColor = kFontBlack}) {
+  return Icon(Icons.accessibility, color: mColor, size: 25);
+}
