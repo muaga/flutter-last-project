479bed84a2eefc8d8f69ced228b2efd93eff1bf8
muaga <more9494@naver.com>
2023-10-25 04:10:56 +0900

    화면 ê구현중

--- lib/_core/constants/move.dart
+++ lib/_core/constants/move.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blog/ui/pages/auth/join_id_email_page/join_id_email_page.dart';
+import 'package:flutter_blog/ui/pages/auth/login_or_sign_page/login_or_sign_page.dart';
 import 'package:flutter_blog/ui/pages/auth/millie_splash_page/millie_splash_page.dart';
 import 'package:flutter_blog/ui/pages/custom/book_detail_page/book_detail_page.dart';
 import 'package:flutter_blog/ui/pages/custom/post_detail_page/post_detail_page.dart';
@@ -7,14 +8,15 @@ import 'package:flutter_blog/ui/pages/custom/post_write_page/post_write_page.dar
 import 'package:flutter_blog/ui/pages/today_now/now_book_list_page/now_book_list_page.dart';
 
 class Move {
-  // TODO : 서버 측 엔드포인트 확인
-  // 예시
+  // TODO : 서버 측 엔드포인트 확인 -> 변경하기
+
   static String NowBookListPage = "/nowBooks";
   static String BookDetailPage = "/bookDetail/1";
   static String PostDetailPage = "/postDetail";
   static String PostWritePage = "/postWrite";
   static String JoinIdEmailPage = "/joinIdEmail";
   static String MainSplashPage = "/mainSplash";
+  static String LoginOrSignPage = "/loginJoin";
 }
 
 Map<String, Widget Function(BuildContext)> getRouters() {
@@ -26,5 +28,6 @@ Map<String, Widget Function(BuildContext)> getRouters() {
     Move.PostWritePage: (context) => PostWritePage(),
     Move.JoinIdEmailPage: (context) => const JoinIdEmailPage(),
     Move.MainSplashPage: (context) => const MainSplashPage(),
+    Move.LoginOrSignPage: (context) => const LoginOrSignPage(),
   };
 }
--- lib/ui/pages/auth/login_or_sign_page/login_or_sign_page.dart
+++ lib/ui/pages/auth/login_or_sign_page/login_or_sign_page.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/ui/pages/auth/login_or_sign_page/widgets/login_or_sign_body.dart';
 
 class LoginOrSignPage extends StatelessWidget {
   const LoginOrSignPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/login2.jpg"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: LoginOrSignBody(),
+      ),
+    );
   }
 }
