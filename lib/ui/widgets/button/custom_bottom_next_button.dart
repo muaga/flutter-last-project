b4095897134f111facef247570324b90e9da5003
wlstjdgns <jin009471@naver.com>
2023-10-24 19:46:21 +0900

    회원가입 통신중

--- lib/ui/widgets/button/custom_bottom_next_button.dart
+++ lib/ui/widgets/button/custom_bottom_next_button.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CustomBottomNextButton extends StatelessWidget {
-  const CustomBottomNextButton({
-    super.key,
-  });
+  final String buttontext;
+  final funPageRoute;
+  const CustomBottomNextButton(
+      {required this.buttontext, required this.funPageRoute});
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-        onPressed: () {},
-        child: Text("다음"),
+        onPressed: funPageRoute,
+        child: Text("${buttontext}"),
         style: ElevatedButton.styleFrom(
             minimumSize: Size(double.infinity, 60),
             shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero)));
