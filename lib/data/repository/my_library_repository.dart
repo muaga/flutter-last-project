c198171b635716aa09c21a192429f48b5c7cb27f
saki8661 <saki8661@naver.com>
2023-11-12 01:14:42 +0900

    내 서재 통신

---
+++ lib/data/repository/my_library_repository.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_blog/_core/constants/http.dart';
+import 'package:flutter_blog/data/dto/response_dto/reponse_dto.dart';
+import 'package:flutter_blog/ui/pages/my_library/my_libray_main_page/widgets/my_library_main_view_model.dart';
+
+class MyLibraryRepository {
+  // 피드 전체 post 목록
+  Future<ResponseDTO> fetchMyLibraryInfo(String jwt) async {
+    try {
+      // 1. 통신
+      Response response = await dio.get("/myLibraryMain",
+          options: Options(headers: {"Authorization": jwt}));
+
+      // 2. ResponseDTO 파싱
+      ResponseDTO responseDTO = ResponseDTO.fromJson(response.data);
+
+      MyLibraryMainModel model = MyLibraryMainModel.fromJson(responseDTO.data);
+      responseDTO.data = model;
+
+      return responseDTO;
+    } catch (e) {
+      return ResponseDTO(-1, "내 서재 불러오기 실패", null);
+    }
+  }
+}
