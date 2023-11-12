a0d52b6ecd92b4d63e511f14dab2f11bc2b66241
muaga <more9494@naver.com>
2023-11-12 10:29:18 +0900

    통센신테스트는 í완료

--- lib/ui/pages/today_now/one_month_press_book_list_page/widgets/body/one_month_press_book_list_body.dart
+++ lib/ui/pages/today_now/one_month_press_book_list_page/widgets/body/one_month_press_book_list_body.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blog/ui/widgets/form/custom_category_form.dart';
+import 'package:flutter_blog/ui/pages/today_now/one_month_press_book_list_page/widgets/one_month_category_form.dart';
 
 class OneMonthPressBookListBody extends StatelessWidget {
   const OneMonthPressBookListBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CustomCategoryForm();
+    return OneMonthCategoryForm();
   }
 }
